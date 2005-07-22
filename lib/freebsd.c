b70efbab5d0760e45f519a7b49f8de278bfbc2fc
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2005-07-22 12:10:10 +0000

    Added 'fake' code for FreeBSD support.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk/unieject@26 da1bf7b7-c300-0410-a191-dd0de0000cc0

---
+++ lib/freebsd.c
@@ -0,0 +1,30 @@
+/* unieject - Universal eject command
+   Copyright (C) 2005, Diego Pettenò
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   */
+
+#include <unieject_internal.h>
+
+char *checkmount(const char *progname, struct unieject_opts opts, char **device)
+{
+	return *device;
+}
+
+bool internal_umountdev(char *progname, struct unieject_opts opts, const char *device)
+{
+	return true;
+}
