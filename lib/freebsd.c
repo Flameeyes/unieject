ff09ef015496048267c2636999e2dccb31acd16e
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2005-07-22 14:53:19 +0000

    Implemented FreeBSD's lookup of mounted devices.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk/unieject@29 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- lib/freebsd.c
+++ lib/freebsd.c
@@ -15,13 +15,59 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+   Portions of this files are under the following copyrights:
+     Copyright (c) 1995-2000 Shunsuke Akiyama <akiyama@FreeBSD.org>.
+
    */
 
 #include <unieject_internal.h>
 
+#include <sys/param.h>
+#include <sys/mount.h>
+
 char *checkmount(const char *progname, struct unieject_opts opts, char **device)
 {
-	return *device;
+	char *ret = NULL;
+	struct statfs *mntbuf;
+	int mnts = getmntinfo(&mntbuf, MNT_NOWAIT);
+	if (mnts == 0)
+	{
+		unieject_error(stderr, "%s: error receiving mount information: %s\n", progname, strerror(errno));
+		return *device;
+	}
+	
+	for (int n = 0; n < mnts; n++)
+	{
+		// ignore special devices
+		if ( mntbuf[n].f_mntfromname[0] != '/' ) continue;
+		
+		char *newdev = simplifylink(progname, mntfbuf[n].f_mntfromname);
+		char *newmnt = simplifylink(progname, mntfbuf[n].f_mntonname);
+		
+		if ( strcmp(newdev, *device) == 0 )
+		{
+			unieject_verbose(stdout, "%s: '%s' is mounted as '%s'\n", progname, *device, newmnt);
+			ret = (newmnt == mntfbuf[n].f_mntonname) ? sstrdup(mntfbuf[n].f_mntonname) : newmnt;
+			
+			if ( newdev != mntfbuf[n].f_mntfromname ) free(newdev);
+			break;
+		}
+
+		if ( strcmp(newmnt, *device) == 0 )
+		{
+			unieject_verbose(stdout, "%s: '%s' is the mount point of '%s'\n", progname, *device, newdev);
+			ret = *device;
+			*device = (newdev == mntfbuf[n].f_mntfromname) ? sstrdup(mntfbuf[n].f_mntfromname) : newdev;
+			
+			if ( newmnt != mntfbuf[n].f_mntonname ) free(newmnt);
+			break;
+		}
+		
+		if ( newdev != mntfbuf[n].f_mntfromname ) free(newdev);
+		if ( newmnt != mntfbuf[n].f_mntonname ) free(newmnt);
+	}
+	
+	return ret;
 }
 
 bool internal_umountdev(char *progname, struct unieject_opts opts, const char *device)
