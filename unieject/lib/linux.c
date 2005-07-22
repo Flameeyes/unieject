c8d469bcea8ccc91a1f3527303ce97e4ae403845
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2005-07-22 10:16:19 +0000

    Added preliminary mountpoint support. Moved link check support in utils.c
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk@20 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- unieject/lib/linux.c
+++ unieject/lib/linux.c
@@ -17,30 +17,43 @@
 
    */
 
-#ifndef __UNIEJECT_INTERNAL_H__
-#define __UNIEJECT_INTERNAL_H__
-
-// We need this for asprintf
-#define _GNU_SOURCE
-
-#define unieject_error \
-	if ( opts.verbose != -1 ) fprintf
-
-#define unieject_verbose \
-	if ( opts.verbose == 1 ) fprintf
-
-#include <config.h>
-#include <string.h>
-
+#include <unieject_internal.h>
 #include <unieject.h>
 
-// safe strdup
-static char *sstrdup(const char *str)
+#include <stdio.h>
+
+char *checkmount(const char *progname, struct unieject_opts opts, const char **device)
 {
-	return str ? strdup(str) : NULL;
+	char *ret = NULL;
+	FILE *fp = fopen("/proc/mounts", "r");
+	
+	char *mnt = (char*)malloc(1024);
+	char *dev = (char*)malloc(1024);
+	
+	while ( fscanf(fp, "%s %s %s* %s* %d* %d*\n", dev, mnt) != EOF )
+	{
+		// symlinks?
+		if ( strcmp(dev, *device) == 0 )
+		{
+			unieject_verbose(stdout, "%s: '%s' is mounted as '%s'", progname, *device, mnt);
+			ret = sstrdup(mnt);
+			break;
+		}
+		
+		// traling / ?
+		if ( strcmp(mnt, *device) == 0 )
+		{
+			unieject_verbose(stdout, "%s: '%s' is the mount point of '%s'\n", progname, *device, dev);
+			ret = *device;
+			*device = sstrdup(dev);
+			
+			break;
+		}
+	
+	}
+	free(mnt);
+	free(dev);
+	
+	fclose(fp);
+	return ret;
 }
-
-char *simplifylink(const char *progname, const char *link);
-char *checkmount(const char *progname, struct unieject_opts opts, const char **device);
-
-#endif
