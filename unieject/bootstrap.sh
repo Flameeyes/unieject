09e09374ec0207dc726e00a96cfb912e2d12d9e8
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2005-07-23 18:02:00 +0000

    Fix bootstrap to work on OSX and use INCLUDES to set the include path.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk@62 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- unieject/bootstrap.sh
+++ unieject/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export WANT_AUTOCONF_2_5="yes"
+export WANT_AUTOMAKE_1_9="yes"
+export WANT_AUTOCONF="2.5"
+export WANT_AUTOMAKE="1.9"
+
 touch ChangeLog NEWS
 libtoolize --copy --force
-aclocal -I m4 --force
+aclocal -I m4
 autoconf --force
 autoheader --force
 automake --add-missing --force-missing  --copy
