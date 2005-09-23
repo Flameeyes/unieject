445a3e6d6232d53239c2d4673d5d3ff1c8236cdc
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2005-09-23 10:11:31 +0000

    Added workaround for Linux locking, as the raw MMC command is not allowed by default.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk@144 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- unieject/lib/lock.c
+++ unieject/lib/lock.c
@@ -24,32 +24,69 @@
 
 #include <stdio.h>
 
+#if defined(__linux__) && defined(USE_LOCK_WORKAROUND)
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <linux/cdrom.h>
+
+# ifndef CDROM_LOCKDOOR
+#  error "Unable to find CDROM_LOCKDOOR definition, and lock workaround requested"
+# endif
+#endif
+
 /* Set how long to wait for MMC commands to complete */
 #define DEFAULT_TIMEOUT_MS 100000
 
-int libunieject_togglelock(struct unieject_opts opts, int lock)
+int libunieject_togglelock(struct unieject_opts *opts, int lock)
 {
-	if ( opts.caps )
+	if ( opts->caps )
 	{
 		// TODO: tell libcdio author about this
 		cdio_drive_misc_cap_t unused, misc_cap;
-		cdio_get_drive_cap((CdIo_t*)opts.cdio, &unused, &unused, &misc_cap);
+		cdio_get_drive_cap((CdIo_t*)opts->cdio, &unused, &unused, &misc_cap);
 
 #ifdef __FreeBSD__
-		if ( strncmp("/dev/cd", opts.device, 7) != 0 )
+		if ( strncmp("/dev/cd", opts->device, 7) != 0 )
 			misc_cap = 0xFFFFFFFF;
 #endif
 
 		if ( ! (misc_cap & CDIO_DRIVE_CAP_MISC_LOCK) )
 		{
-			unieject_error(opts, _("the selected device doesn't have locking capabilities.\n"));
+			unieject_error(*opts, _("the selected device doesn't have locking capabilities.\n"));
 			return -2;
 		}
 	}
 	
-	if ( opts.fake )
+	if ( opts->fake )
 		return 0;
 
+/*
+ * On Linux the allow medium removal command is a priviledged command that
+ * cannot be instanced by userspace.
+ * Until this changes, use a workaround with syscalls instead.
+ */
+#if defined(__linux__) && defined(USE_LOCK_WORKAROUND)
+	cdio_destroy(opts->cdio);
+	opts->cdio = NULL;
+	
+	int devfd = open(opts->device, O_RDONLY|O_NONBLOCK);
+	if ( devfd == -1 )
+	{
+		unieject_error(*opts, _("unable to open device descriptor [%s].\n"), strerror(errno));
+		return -4;
+	}
+	
+	if ( ioctl(devfd, CDROM_LOCKDOOR, lock ? 1 : 0) == -1 )
+	{
+		unieject_error(*opts, _("error in ioctl [%s].\n"), strerror(errno));
+		return -5;
+	}
+	
+	close(devfd);
+	
+	return 0;
+#else
 	mmc_cdb_t lockcmd = { {0, } }; /* Command description buffer */
 	uint8_t buf[1]; /* Fake buffer */
 	
@@ -57,10 +94,8 @@ int libunieject_togglelock(struct unieject_opts opts, int lock)
 	
 	lockcmd.field[4] = lock ? 1 : 0;
 	
-	fprintf(stderr, "DEBUG: %02x%02x%02x%02x%02x%02x\n", lockcmd.field[0], lockcmd.field[1], lockcmd.field[2], lockcmd.field[3], lockcmd.field[4], lockcmd.field[5]);
-	
 	driver_return_code_t sts = mmc_run_cmd(
-		(CdIo_t*)opts.cdio,
+		(CdIo_t*)opts->cdio,
 		DEFAULT_TIMEOUT_MS, /* Should this be configurable? */
 		&lockcmd,
 		SCSI_MMC_DATA_READ,
@@ -68,5 +103,6 @@ int libunieject_togglelock(struct unieject_opts opts, int lock)
 		buf
 		);
 	
-	return unieject_status(opts, sts);
+	return unieject_status(*opts, sts);
+#endif
 }
