5a98c61ec8b0daf8bb9f26395e75fc1735b2f828
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2006-06-16 12:02:22 +0000

    Error out rather than segfaulting when no device at all can be found.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk/unieject@226 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- lib/mounts.c
+++ lib/mounts.c
@@ -60,7 +60,10 @@ char *libunieject_getdevice(struct unieject_opts opts, const char *basename)
 	if ( ! normalized )
 	{
 		normalized = libunieject_defaultdevice();
-		unieject_verbose(opts, _("using default device '%s'\n"), normalized);
+		if ( ! normalized )
+			unieject_error(opts, _("no default device identified, exiting."));
+		else
+			unieject_verbose(opts, _("using default device '%s'\n"), normalized);
 	}
 
 	unieject_verbose(opts, _("device name is '%s'\n"), normalized);
