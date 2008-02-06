e4c987462511d5e95d86c2c527c24d267cc9c11e
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-02-06 20:34:16 +0000

    Use automake 1.10 rather than 1.9.
    git-svn-id: file:///home/flame/mytmpfs/unieject/trunk/unieject@295 da1bf7b7-c300-0410-a191-dd0de0000cc0

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export WANT_AUTOCONF_2_5="yes"
-export WANT_AUTOMAKE_1_9="yes"
+export WANT_AUTOMAKE_1_10="yes"
 export WANT_AUTOCONF="2.5"
-export WANT_AUTOMAKE="1.9"
+export WANT_AUTOMAKE="1.10"
 
 touch ChangeLog
 libtoolize --force
